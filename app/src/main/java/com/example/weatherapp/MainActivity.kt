e0060c072efd186f021dae6eef03cfae81e0d110
Albert Brozyna <245779@edu.p.lodz.pl>
2025-04-10 01:53:29 +0200

    Project init.

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.weatherapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.weatherapp.ui.theme.WeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                    WeatherScreen()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun WeatherScreen(){
+    var city = remember { mutableStateOf("") }
+    var temperature = remember { mutableStateOf<String?>(null) }
+
+    Column (
+        modifier = Modifier.
+                fillMaxSize()
+                .padding(16.dp)
+    ){
+        TextField(
+            value = city.value,
+            onValueChange = { city.value = it },
+            label = { Text("City") },
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+
+
+}
+
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WeatherAppTheme {
+        Greeting("Android")
+    }
+}
